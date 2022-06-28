118c122b727f9f735bdf29607f9743a266518bb7
Taguhi <taguhikarakash@gmail.com>
2022-06-28 15:59:52 +0200

    folder changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Posts from './container/Posts';
 import './index.css';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Posts />
   </React.StrictMode>
 );
 
